6df50c9b0a13db1bd9a93667f2a16a36eff26086
marijnschilling <marijn.schilling@payconiq.com>
2021-02-04 15:06:10 +0100

    Create Project

---
+++ ClimateCombat/ClimateCombat/ClimateCombatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateCombatApp.swift
+//  ClimateCombat
+//
+//  Created by Marijn Schilling on 04/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ClimateCombatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
